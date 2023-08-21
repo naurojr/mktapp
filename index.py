a8d2b615f5b2bcc74854753a8672bd6a19be01eb
Nauro Rezende Jr <naurojr@therebelidea.com>
2023-08-21 15:41:33 -0400

    Load librariest to test

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import os.path
+import pathlib
+import pandas as pd
+
+from io import StringIO
+from pathlib import Path
+
+
+import yaml
+from yaml.loader import SafeLoader
 
 st.title('Alltomator')
