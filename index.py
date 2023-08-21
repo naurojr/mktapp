a4745874cbc70f3093e2d9e93da7db1ed5f5e972
Nauro Rezende Jr <naurojr@therebelidea.com>
2023-08-21 16:03:16 -0400

    loading credentials

--- index.py
+++ index.py
@@ -20,3 +20,13 @@ with open('./config.yaml') as file:
 	config = yaml.load(file, Loader=SafeLoader)
 
 st.title("Marketing Tools :toolbox:", anchor=None)
+
+authenticator = stauth.Authenticate(
+		config['credentials'],
+		config['cookie']['name'],
+		config['cookie']['key'],
+		config['cookie']['expiry_days'],
+		config['preauthorized']
+	)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
